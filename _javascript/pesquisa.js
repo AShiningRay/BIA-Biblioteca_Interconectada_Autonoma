4e5cda748a15a011f5a6047e6727c65701373ab1
AShiningRay <genesis45.ppc@gmail.com>
2022-02-16 20:10:41 -0300

    Consertando bugs e adicionando funcionalidades na pesquisa.

--- _javascript/pesquisa.js
+++ _javascript/pesquisa.js
@@ -1,14 +1,18 @@
-$("#staticBackdrop").load("./login.html");
-$("#mainnavbar").load("./navbar.html");
-$("#mainfooter").load("./footer.html");
+$(document).ready(function() {
+    $("#staticBackdrop").load("./login.html");
+    $("#mainnavbar").load("./navbar.html");
+    $("#mainfooter").load("./footer.html");
 
-// Button press skeleton functions
-$("#loginbutton").click(function () {
-    $(".mainpage").css("filter", "blur(5px)");
-    console.log("blurred");
-});
-$(document).on('click', '#closelogwindow', function () {
-    $(".mainpage").css("filter", "blur(0px)");
-    console.log("removed blur");
-});
+    // Button press skeleton functions
+    $("#loginbutton").click(function () {
+        $(".mainpage").css("filter", "blur(5px)");
+    });
+    $(document).on('click', '#closelogwindow', function () {
+        $(".mainpage").css("filter", "blur(0px)");
+    });
 
+    $(document).on('click', '#search_button', function () {
+        let text = $("#search_field").val();
+        console.log("Pesquisando por: ", text);
+    });
+});
