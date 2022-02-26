83faaf89807f30e02e4e2c9f56b4e1941edd0409
AShiningRay <genesis45.ppc@gmail.com>
2022-02-26 14:36:41 -0300

    Reorganizando os arquivs da pagina de contato.
    Um arquivo js separado facilita modularizar a funcionalidade da
    pagina.

---
+++ _javascript/contato.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("#staticBackdrop").load("./login.html");
+			$("#mainnavbar").load("./navbar.html");
+			$("#mainfooter").load("./footer.html");
+
+			// Button press skeleton functions
+			$("#loginbutton").click(function () {
+				$(".mainpage").css("filter", "blur(5px)");
+				console.log("blurred");
+			});
+			$(document).on('click', '#closelogwindow', function () {
+				$(".mainpage").css("filter", "blur(0px)");
+				console.log("removed blur");
+	});
+});
