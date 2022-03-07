d97407e3fc45c8b94fff6b3d71e007593b54ccf5
MatheusRFC <matheusr220998@gmail.com>
2022-03-07 16:16:35 -0300

    Comments adicionados na classe de conexão.

--- _php/bd.php
+++ _php/bd.php
@@ -3,7 +3,9 @@ class Connection
 {
     public $connection;
     public function __construct()
-    {
+    {                   
+        //                                   server    user    senha    BD      
+        //                                     ↓         ↓       ↓      ↓
         $this->connection = mysqli_connect("localhost","root", "123", "test");
     }
 }
