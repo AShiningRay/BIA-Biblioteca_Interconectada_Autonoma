5724faa82ba7410ebf35d99993b3d48d3203f03f
AShiningRay <genesis45.ppc@gmail.com>
2022-03-08 20:31:32 -0300

    Adicionando novas tabelas para informacoes de livros.

--- _php/cadastro_livros.php
+++ _php/cadastro_livros.php
@@ -18,10 +18,11 @@ $Ano_public = $_POST['data_pub'];
 $Editora = $_POST['nome_editora'];
 $Referencias = $_POST['referencias'];
 $Localizacao = $_POST['localizacao'];
+$Disponíveis = $_POST['num_disponivel'];
+$SiteImagem = $_POST['site_imagem'];
 
-
-$sql = "INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`, `referencias`, `localizacao`) 
-        VALUES ('$Name', '$Autor', '$Isbn','$Ano_public','$Editora ','$Referencias','$Localizacao')";
+$sql = "INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`, `referencias`, `localizacao`, `disponivel`, `site_imagem`) 
+        VALUES ('$Name', '$Autor', '$Isbn','$Ano_public','$Editora ','$Referencias','$Localizacao', '$Disponíveis', '$SiteImagem')";
 
 
 mysqli_query($conn->connection, $sql);
