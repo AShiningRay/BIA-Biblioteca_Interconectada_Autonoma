c957256f1dcd27e9a55a78f2197ffe8741f078b3
MatheusRFC <matheusr220998@gmail.com>
2022-03-08 18:31:45 -0300

    Arquivo de dados atualizado para testes.

--- _bd/test.sql
+++ _bd/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Mar-2022 às 22:15
+-- Tempo de geração: 08-Mar-2022 às 22:31
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 7.4.27
 
@@ -36,6 +36,14 @@ CREATE TABLE `cadastro_livros` (
   `referencias` varchar(50) DEFAULT NULL,
   `localizacao` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `cadastro_livros`
+--
+
+INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`, `referencias`, `localizacao`) VALUES
+('machado', '', '', '', ' ', '', ''),
+('Memórias Póstumas de Bras Cubas', 'Machado de Assis', '64654465465465', '1881', 'Moderna ', 'Assis, M de.', 'Fileira 3 Seção 2');
 
 -- --------------------------------------------------------
 
