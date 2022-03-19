7ee33c78ff7eb80a6e0d258f7d66f617035c850e
MatheusRFC <matheusr220998@gmail.com>
2022-03-19 13:48:32 -0300

    Arquivo .sql do banco de dados de livros atualizado.

--- _bd/biblioteca.sql
+++ _bd/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16-Mar-2022 às 21:59
+-- Tempo de geração: 19-Mar-2022 às 17:47
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 7.4.27
 
@@ -47,7 +47,7 @@ CREATE TABLE `cadastro_livros` (
 --
 
 INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`, `referencias`, `localizacao`, `livro_categoria`, `livro_edicao`, `livro_volume`, `qtd_disponivel`, `site_imagem`) VALUES
-('Memórias Póstumas de Brás Cubas', 'Machado de Assis', '9780850515022', '1881', 'Editoria Moderna ', 'ASSIS, Machado de. Memórias Póstumas de Brás Cubas', 'Fileira 3 - Seção 2', 'Literatura', '2º Edição', 'Volume 1', '10', 'BrasCubas.png'),
+('Memórias Póstumas de Brás Cubas', 'Machado de Assis', '9780850515022', '1881', 'Editora Moderna', 'ASSIS, Machado de. Memórias Póstumas de Brás Cubas', 'Fileira 3 - Seção 2', 'Literatura', '2º Edição', 'Volume 1', '10', 'BrasCubas.png'),
 ('O Cortiço', 'Aluísio Azevedo', '9780850515015 ', '1890', 'Editora SESI-SP ', 'AZEVEDO, Aluísio. O cortiço. 30. ed. São Paulo: Át', 'Fileira 40 - Seção 6', 'Literatura', '3º Edição', 'Volume 3', '15', 'oCortico.png'),
 ('Sistemas Operacionais Modernos', 'Andrew S. Tanenbaum', '9788543005676', '2016', 'Editora Pearson ', 'Tanenbaum, Andrew S. Sistemas Operacionais Moderno', 'Fileira 17 - Seção 15', 'Computação', '4ª Edição', 'Volume 2', '12', 'SOsModernosTanembaum.png'),
 ('Dom Casmurro', 'Machado de Assis', '9788567097688', '1899', 'Editora Moderna ', 'ASSIS, Machado de. Dom Casmurro. Rio de Janeiro: M', 'Fileira 6 - Seção 23', 'Literatura', '2º Edição', 'Volume 3', '16', 'DomCasmurro.png'),
@@ -56,7 +56,9 @@ INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`,
 ('Biologia celular e molecular', 'Eduardo de Robertis e José Hib', '9789500203845', '1987', 'Guanabara Koogan ', 'DE ROBERTIS, E. M. F.; HIB, Jose. Biologia celular', 'Fileira 1 - Seção 23', 'Biologia', '16º Edição', 'Volume 1', '26', 'BiologiaCelularEMolecular.png'),
 ('A Elite do Atraso: Da Escravidão à Lava Jato', 'Jessé Souza', '9788556080431', '2017', 'Editora Leya ', 'Souza, Jessé de. A elite do atraso: da escravidão ', 'Fileira 23 - Seção 2', 'Sociologia', '1º Edição', 'Volume 2', '16', 'AElitedoAtraso.png'),
 ('Vigiar e Punir', 'Michel Foucault', '9780140137224', '1975', 'Gallimard ', 'FOUCAULT, M. . Vigiar e Punir: história da violênc', 'Fileira 15 - Seção 32', 'Sociologia', '1º Edição', 'Volume 3', '23', 'VigiarEPunir.png'),
-('Sociedade do Cansaço', 'Byung-Chul Han', '9780804797504', '2010', 'Editora Vozes ', 'Han, Byung-Chul. Sociedade do cansaço . Editora Vo', 'Fileira 53 - Seção 12', 'Filosofia', '1º Edição', 'Volume 1', '12', 'SociedadeDoCansaco.png');
+('Sociedade do Cansaço', 'Byung-Chul Han', '9780804797504', '2010', 'Editora Vozes ', 'Han, Byung-Chul. Sociedade do cansaço . Editora Vo', 'Fileira 53 - Seção 12', 'Filosofia', '1º Edição', 'Volume 1', '12', 'SociedadeDoCansaco.png'),
+('História da Filosofia Ocidental', 'Bertrand Russell', '9780041000450', '1945', 'Editora Moderna ', 'Russel, Bertrand. História da Filosofia Ocidental.', 'Fileira 8 - Seção 23', 'Filosofia', '3º Edição', 'Volume 4', '52', 'HistoriaFilosofiaOcidental.png'),
+('Uma Breve História do Tempo', 'Stephen Hawking', '9780593015186', '1988', 'Editora Intrínseca ', 'Hawking, Stephen. Uma Breve História do Tempo. Edi', 'Fileira 22 - Seção 44', 'Física', '7º Edição', 'Volume 1', '23', 'UmaBreveHistoriaDoTempo.png');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
