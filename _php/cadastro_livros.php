b4b50e9cd78c5e1fa84fa05475bafbc1779dc825
MatheusRFC <matheusr220998@gmail.com>
2022-03-19 13:44:28 -0300

    Pequenos comentários adicionados.

--- _php/cadastro_livros.php
+++ _php/cadastro_livros.php
@@ -25,6 +25,7 @@ $Edicao = trim($_POST['edicao']);
 $Volume = trim($_POST['volume']);
 $SiteImagem = trim($_POST['site_imagem']);
 
+//Query para verificação de repetição de livros.
 $sql = "SELECT * FROM cadastro_livros where nome = '$Name' 
                                             and autor = '$Autor' 
                                             and isbn = '$Isbn'
@@ -35,17 +36,22 @@ $sql = "SELECT * FROM cadastro_livros where nome = '$Name'
 
 $resultado = mysqli_query($conn->connection, $sql);
 
+//Se retornar ao menos um resultado, o livro já existe no banco de dados, e não pode ser adicionado de novo.
 if ($resultado->num_rows > 0){
-    echo  "<script>alert('Erro Livro repetido!');</script>";
+    echo  "Livro repetido"; //Não aparece na tela, isso será feito posteriormente.
 }
 
+//Sew o livro não existir no banco de dados, ele é cadastrado normalmente
 else {
     $sql = "INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`, `referencias`, `localizacao`,`livro_categoria`, `livro_edicao`,`livro_volume`, `qtd_disponivel`, `site_imagem`) 
     VALUES ('$Name', '$Autor', '$Isbn','$Ano_public','$Editora ','$Referencias','$Localizacao','$Categoria','$Edicao','$Volume','$Disponíveis', '$SiteImagem')";
 mysqli_query($conn->connection, $sql);
 }
 
+//Fecha a conexão com o bd.
 mysqli_close($conn->connection);
+
+//Retorna a página de cadastro.
 header("Location:../_html/cadastro_livros.html");
 ?>
 
