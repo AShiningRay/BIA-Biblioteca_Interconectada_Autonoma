64345a0444480a66825217435356386f8d72488d
MatheusRFC <matheusr220998@gmail.com>
2022-03-19 13:41:42 -0300

    Função de verificação de repetição de livros adicionada ao cadastro, agora não é possível adicionar livros que contenham nome, autor, isbn, ano de publicação, editora, edição e volume repetidos, visto que isso seria o mesmo livro.
    Novo livro de física adicionado.

--- _php/cadastro_livros.php
+++ _php/cadastro_livros.php
@@ -25,11 +25,25 @@ $Edicao = trim($_POST['edicao']);
 $Volume = trim($_POST['volume']);
 $SiteImagem = trim($_POST['site_imagem']);
 
-$sql = "INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`, `referencias`, `localizacao`,`livro_categoria`, `livro_edicao`,`livro_volume`, `qtd_disponivel`, `site_imagem`) 
-        VALUES ('$Name', '$Autor', '$Isbn','$Ano_public','$Editora ','$Referencias','$Localizacao','$Categoria','$Edicao','$Volume','$Disponíveis', '$SiteImagem')";
+$sql = "SELECT * FROM cadastro_livros where nome = '$Name' 
+                                            and autor = '$Autor' 
+                                            and isbn = '$Isbn'
+                                            and ano_public = '$Ano_public'
+                                            and editora = '$Editora'
+                                            and livro_edicao = '$Edicao'
+                                            and livro_volume = '$Volume'";
 
+$resultado = mysqli_query($conn->connection, $sql);
 
+if ($resultado->num_rows > 0){
+    echo  "<script>alert('Erro Livro repetido!');</script>";
+}
+
+else {
+    $sql = "INSERT INTO `cadastro_livros` (`nome`, `autor`, `isbn`, `ano_public`, `editora`, `referencias`, `localizacao`,`livro_categoria`, `livro_edicao`,`livro_volume`, `qtd_disponivel`, `site_imagem`) 
+    VALUES ('$Name', '$Autor', '$Isbn','$Ano_public','$Editora ','$Referencias','$Localizacao','$Categoria','$Edicao','$Volume','$Disponíveis', '$SiteImagem')";
 mysqli_query($conn->connection, $sql);
+}
 
 mysqli_close($conn->connection);
 header("Location:../_html/cadastro_livros.html");
