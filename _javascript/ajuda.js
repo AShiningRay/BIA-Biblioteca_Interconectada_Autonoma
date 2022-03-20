a02adc8dbf228ec3ebe1f0d5db39be8821c705bd
AShiningRay <genesis45.ppc@gmail.com>
2022-03-20 14:12:39 -0300

    Adicionada funcionalidade de exibicao modular dos blocos de ajuda.
    Agora apenas os blocos relacionados a um certo botao aparecem
    quando o usuario clica neles.

---
+++ _javascript/ajuda.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#generalnavbutton').click(function () {
+        $('.generalnav').css('display', 'block');
+        $('.startpage').css('display', 'none');
+        $('.booksearch').css('display', 'none');
+        $('.dologin').css('display', 'none');
+    });
+
+    $('#startpagebutton').click(function () {
+        $('.generalnav').css('display', 'none');
+        $('.startpage').css('display', 'block');
+        $('.booksearch').css('display', 'none');
+        $('.dologin').css('display', 'none');
+    });
+
+    $('#booksearchbutton').click(function () {
+        $('.generalnav').css('display', 'none');
+        $('.startpage').css('display', 'none');
+        $('.booksearch').css('display', 'block');
+        $('.dologin').css('display', 'none');
+    });
+
+    $('#dologinbutton').click(function () {
+        $('.generalnav').css('display', 'none');
+        $('.startpage').css('display', 'none');
+        $('.booksearch').css('display', 'none');
+        $('.dologin').css('display', 'block');
+    });
+});
